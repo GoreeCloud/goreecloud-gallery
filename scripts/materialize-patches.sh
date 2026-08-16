177fe650e0d4f627af65eb364c22e310c9cd170d
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-16 18:00:22 -0500

    Correct gc.4 provenance while adding gc.12

--- scripts/materialize-patches.sh
+++ scripts/materialize-patches.sh
@@ -50,7 +50,7 @@ materialize() {
 materialize gc1 build_goreecloud_gallery.py      100b2080cf5e82275dce7a1a1f35d8869ab8af38
 materialize gc2 build_goreecloud_gallery_gc2.py  5a9d84b0eaa49107cba52f2b1f02131fa5d03f3e
 materialize gc3 build_goreecloud_gallery_gc3.py  67a4e4acfebb2c6e3d271d5387ededa60bc0ee87
-materialize gc4 build_goreecloud_gallery_gc4.py  e8b1362e87d0e47997fa7f2ee36f851b4123fab5
+materialize gc4 build_goreecloud_gallery_gc4.py  8f3a48d424ead1c253e0e9eb91f27706e6162757
 materialize gc5 build_goreecloud_gallery_gc5.py  e8b1362e87d0e47997fa7f2ee36f851b4123fab5
 materialize gc6 build_goreecloud_gallery_gc6.py  4c9094e7b4139e0472f1c17ab2ff4a2186244c78
 materialize gc7 build_goreecloud_gallery_gc7.py  516339487492806932ec14b669c183e4919b1187
